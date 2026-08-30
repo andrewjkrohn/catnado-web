0fbc2234c19b1a885a7b0195639df8cce161d877
User <andrew@flomatik.ai>
2026-08-29 23:42:26 -0500

    Catnado web build 0.1.33

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788060574|13803633';
+const CACHE_VERSION = '1788064944|13483501';
 /** @type {string} */
 const CACHE_PREFIX = 'Catnado-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
